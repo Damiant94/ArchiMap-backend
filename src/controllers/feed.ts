59ee65a73332897f51844717f72da3955d0de0c3
Damian Tulacz <damian.tulacz@gmail.com>
2024-06-17 21:53:52 +0200

    updated getObjects endpoint, added query params for filters

--- src/controllers/feed.ts
+++ src/controllers/feed.ts
@@ -17,13 +17,13 @@ export const addObject = (req: Request, res: Response, next: NextFunction) => {
   const newObject = new ObjectModel<ObjectData>({ ...req.body });
   newObject
     .save()
-    .then((result) => {
+    .then(() => {
       res.status(201).json({
         message: "Object created successfully!",
         newObject: newObject,
       });
     })
-    .catch((err) => {
+    .catch((err: ResponseError) => {
       if (!err.statusCode) {
         err.statusCode = 500;
       }
@@ -32,11 +32,22 @@ export const addObject = (req: Request, res: Response, next: NextFunction) => {
 };
 
 export const getObjects = (req: Request, res: Response, next: NextFunction) => {
-  ObjectModel.find()
-    .then((objects) => {
+  const { querySearch, category, country } = req.query;
+  let query: any = {};
+  if (country) {
+    query["location.country"] = { $regex: country, $options: "i" };
+  }
+  if (querySearch) {
+    query["name"] = { $regex: querySearch, $options: "i" };
+  }
+  if (category) {
+    query["category"] = { $regex: category, $options: "i" };
+  }
+  ObjectModel.find(query)
+    .then((objects: ObjectData[]) => {
       res.status(200).json(objects);
     })
-    .catch((err) => {
+    .catch((err: ResponseError) => {
       if (!err.statusCode) {
         err.statusCode = 500;
       }
@@ -47,7 +58,7 @@ export const getObjects = (req: Request, res: Response, next: NextFunction) => {
 export const getObject = (req: Request, res: Response, next: NextFunction) => {
   const objectId = req.params.objectId;
   ObjectModel.findById(objectId)
-    .then((object) => {
+    .then((object: ObjectData | null) => {
       if (!object) {
         const error = new Error("Could not find object.") as ResponseError;
         error.statusCode = 404;
@@ -55,7 +66,7 @@ export const getObject = (req: Request, res: Response, next: NextFunction) => {
       }
       res.status(200).json(object);
     })
-    .catch((err) => {
+    .catch((err: ResponseError) => {
       if (!err.statusCode) {
         err.statusCode = 500;
       }
@@ -63,21 +74,25 @@ export const getObject = (req: Request, res: Response, next: NextFunction) => {
     });
 };
 
-export const getCountries = (req: Request, res: Response, next: NextFunction) => {
+export const getCountries = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   ObjectModel.find()
-    .then((objects) => {
+    .then((objects: ObjectData[]) => {
       const countries: string[] = [];
-      objects.forEach(({location}) => {
+      objects.forEach(({ location }) => {
         if (!countries.includes(location.country)) {
           countries.push(location.country);
         }
-      })
+      });
       return countries;
     })
-    .then((countries) => {
+    .then((countries: string[]) => {
       res.status(200).json(countries);
     })
-    .catch((err) => {
+    .catch((err: ResponseError) => {
       if (!err.statusCode) {
         err.statusCode = 500;
       }
