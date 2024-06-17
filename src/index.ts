b17aa9cdf306f885ef7ca5805bb425c1bee3dd6e
Damian Tulacz <damian.tulacz@gmail.com>
2024-06-17 21:53:06 +0200

    updated header

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,7 @@ const port = process.env.PORT;
 
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-  const origin = process.env.NODE_ENV === "prod" ? "https://archimap-frontend.onrender.com" : "*";
-  res.setHeader("Access-Control-Allow-Origin", origin);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
