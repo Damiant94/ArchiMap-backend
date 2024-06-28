4655d27eb64d25d16c266f63fd3c7d218c57487d
Damian Tulacz <damian.tulacz@gmail.com>
2024-06-28 16:49:11 +0200

    added interval with get request to self every 5 min

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 import express, { Express, Request, Response, NextFunction } from "express";
 import http from "http";
+import https from "https";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 // import multer from "multer";
@@ -10,7 +11,6 @@ import feedRoutes from "./routes/feed";
 
 const app: Express = express();
 const port = process.env.PORT;
-
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -31,6 +31,20 @@ app.use((error: any, req: Request, res: Response, next: NextFunction) => {
   const data = error.data;
   res.status(status).json({ message: message, data: data });
 });
+
+setInterval(() => {
+  (async () => {
+    try {
+      const response = fetch(
+        "https://archimap-backend.onrender.com/feed/get-countries"
+      );
+      const countries = await (await response).json();
+      console.log(countries);
+    } catch (error) {
+      console.log(error);
+    }
+  })();
+}, 300000);
 
 mongoose
   .connect(
